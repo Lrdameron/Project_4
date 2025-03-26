ef10ad03e60111b19f1a8f17128dcec74954a011
Lrdameron <Lrdameron@gmail.com>
2025-03-25 19:35:30 -0500

    Logan Updates

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import Flask
+from predict_route import predict_bp
+
+app = Flask(__name__)
+app.register_blueprint(predict_bp)
+
+@app.route('/')
+def home():
+    return 'MARYANN THIS IS A PLACEHOLDER FOR ACTUAL TEXT'
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
